0f2d71bba654c98325886ac6f45801da4a7cd223
mailson <mailson.anselmo@gmail.com>
2023-06-21 22:52:29 -0300

    configurando lista

--- src/app/components/real-all/real-all.component.ts
+++ src/app/components/real-all/real-all.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
 
 @Component({
   selector: 'app-real-all',
   templateUrl: './real-all.component.html',
   styleUrls: ['./real-all.component.css']
 })
-export class RealAllComponent {
+export class RealAllComponent implements OnInit{
+
+  list: Todo[] = [
+    {
+      titulo: "Teste1",
+      dataFinalizar: new Date,
+      finalizar: false
+    },
+    {
+      titulo: "Teste2",
+      dataFinalizar: new Date,
+      finalizar: true
+    }
+  ]
+
+
+
+
+  constructor(){ }
+
+  ngOnInit(): void {
+    
+  }
+  
 
 }
